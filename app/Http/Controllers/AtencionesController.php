9196bb3a1f0ddfe40ffebd91154a0919b63b6a3f
zerpaerik <zerpaerik@gmail.com>
2021-08-02 21:37:36 -0400

    aaa

--- app/Http/Controllers/AtencionesController.php
+++ app/Http/Controllers/AtencionesController.php
@@ -1227,7 +1227,7 @@ return view('atenciones.particular');
                               while ($contador < $cantidad) {
                                   $con = new Consultas();
                                   $con->id_paciente =  $request->paciente;
-                                  $con->id_especialista =  100000097;
+                                  $con->id_especialista =  39;
                                   $con->id_atencion =  $lab->id;
                                   $con->tipo =  1;
                                   $con->monto = 0;
@@ -1263,7 +1263,7 @@ return view('atenciones.particular');
                               while ($contador < $cantidad) {
                                   $con = new Consultas();
                                   $con->id_paciente =  $request->paciente;
-                                  $con->id_especialista =  100000097;
+                                  $con->id_especialista =  39;
                                   $con->id_atencion =  $lab->id;
                                   $con->tipo =  2;
                                   $con->monto = 0;
