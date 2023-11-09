e51902da383825d2f8f209dede33850a1278dbdb
Erik Zerpa <zerpaerik@gmail.com>
2023-11-09 10:23:31 -0400

    pacientes

--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -173,8 +173,8 @@
   </div>
   </div>
 
-  <div class="modal fade" id="viewTicket">
-        <div class="modal-dialog">
+  <div class="modal fade" id="viewTicket" style="">
+        <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
