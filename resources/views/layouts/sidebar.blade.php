1f91ec6f970bb92d51ea12eb40c28eb7d80f7d80
zerpaerik <zerpaerik@gmail.com>
2024-04-04 18:53:54 -0400

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1991,6 +1991,148 @@
     </div>
     @endif
 
+             @if(\Auth::user()->rol == 13)
+
+    
+<a href="{{route('home')}}" class="brand-link">
+<img src="logo.jpeg" class="img-circle elevation-2" alt="User Image" width="40">
+      
+      <span class="brand-text font-weight-light">ADMIN MadreTeresa</span>
+    </a>
+
+<div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+    
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+          
+            <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-archive"></i>
+              <p>
+                Archivo
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+        
+              
+              <li class="nav-item">
+                <a href="{{route('analisis.index')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Analisis</p>
+                </a>
+              </li>
+            
+              
+              
+            </ul>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+               Resultados
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+
+              <li class="nav-item">
+                <a href="{{route('resultados.index1')}}" class="nav-link">
+                  <i class="fas fa-cart-arrow-down nav-icon"></i>
+                  <p>Pendientes Laboratorio</p>
+                </a>
+              </li>
+
+           
+
+
+              <li class="nav-item">
+                <a href="{{route('resultados.indexg1')}}" class="nav-link">
+                  <i class="fas fa-cart-arrow-down nav-icon"></i>
+                  <p>Guardados Laboratorio</p>
+                </a>
+              </li>
+
+            
+            
+              
+            </ul>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+                Reportes
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+
+            <li class="nav-item">
+                <a href="{{route('historial.pacientes')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Historial de Pacientes</p>
+                </a>
+              </li>
+
+             
+
+           
+            </ul>
+          </li>
+
+          
+         
+        
+
+    
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tools"></i>
+              <p>
+                Administrativo
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+            <li class="nav-item">
+                <a href="{{route('users.password')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Modificar Contraseña</p>
+                </a>
+              </li>
+              
+              
+            </ul>
+          </li>
+
+
+          
+         
+          
+         
+         
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    @endif
+
 
 
 
